03e5c0a29c274183b43bf103d7fd3f71401c945a
Mateusz Ziebura <mateusz_860@op.pl>
2023-01-24 19:56:14 +0100

    init

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Mateusz");
+
+        int mojaPierwszaLiczba = (10+5)+(2*10);
+        int mojaDrugaLiczba = 12;
+        int mojaTrzeciaLiczba = mojaPierwszaLiczba*2;
+        int mojaSuma = mojaPierwszaLiczba + mojaDrugaLiczba + mojaTrzeciaLiczba;
+        int mojaOstatnia = mojaSuma - 1000;
+        System.out.println(mojaOstatnia);
+
+    }
+
+}
